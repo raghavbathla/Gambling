c893bc15f9786fecc18edb03b394dc0c81c047f4
Raghav <bathla.raghav5@gmail.com>
2022-11-28 16:17:47 +0530

    Any message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
 
+    static int stake = 100;
+    static int bet = 1;
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to gambler program");
     }
